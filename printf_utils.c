8a927b60ebeeccc7c2fa79c40994ad40a13170d0
Daniel Weah <weahjrdanielk@gmail.com>
2025-07-01 11:40:50 +0000

    feat: added the print_string helper function

--- printf_utils.c
+++ printf_utils.c
@@ -1,6 +1,27 @@
-
+#include "main.h"
 /**
  * This file is going to hold our helper functions 
  * will be linked to our main function
  *
  */
+
+ /**
+ * print_string - Prints a string
+ * @args: Argument list
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+    char *str = va_arg(args, char *);
+    int count = 0;
+    if (str == NULL)
+    {
+        str = "(null)";
+    }
+    while (*str)
+    { 
+        count += _putchar(*str);
+        str++;
+    }
+    return count;
+}
